6ab60f06f676f9627db0ce07949e2d2e43ec418a
Anna Jimenez <jimenezpetrushka44@gmail.com>
2025-11-19 08:06:04 -0600

    Fix User property initialization in BillingInfo

--- BillingInfo.cs
+++ BillingInfo.cs
@@ -4,9 +4,8 @@ namespace C_FinallPROJECT.Models
     {
         public int BillingID { get; set; }
         public int UserID { get; set; }
-        public Users User { get; set; } = null!;
         public string CardNumber { get; set; } = null!;
-
+        public Users User { get; set; } = null!;
 
     }
 }
