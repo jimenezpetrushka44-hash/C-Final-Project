c919cd5ce8ef67df4d0a47ef085be2c3a4ff2369
Anna Jimenez <jimenezpetrushka44@gmail.com>
2025-11-19 08:08:44 -0600

    Remove duplicate FloorStatus property

--- Floors.cs
+++ Floors.cs
@@ -5,7 +5,7 @@ namespace C_FinallPROJECT.Models
         public int FloorID { get; set; }
         public int HotelID { get; set; }
         public string FloorStatus { get; set; } = "Available";
-        public string FloorStatus { get; set; } = "Available";
+     
 
         public List<Rooms>? Room { get; set; }
     }
