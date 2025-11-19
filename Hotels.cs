91cb4d6ab79d09e65832c9783096b4d0c308d862
Anna Jimenez <jimenezpetrushka44@gmail.com>
2025-11-19 07:54:59 -0600

    Add Hotels class with properties

---
+++ Hotels.cs
@@ -0,0 +1,12 @@
+namespace C_FinallPROJECT.Models
+{
+    public class Hotels
+    {
+        public int HotelID { get; set; }
+        public string HotelName { get; set; } = null!;
+        public string HotelLocation { get; set; } = null!;
+        public string HotelStatus { get; set; } = "Open";
+
+
+    }
+}
